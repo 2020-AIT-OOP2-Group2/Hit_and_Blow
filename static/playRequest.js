4065f8e1fe2f4580e0c205e64ac6b2216790731c
k19064 <ttt562777@icloud.com>
2020-12-10 19:20:30 +0900

    入力の判定の修正
    文字が含まれているか
    桁数があっているか
    ユニークかどうか

--- static/playRequest.js
+++ static/playRequest.js
@@ -10,26 +10,44 @@ fetch("/start").then(response => {
     });
 });
 
-document.querySelector("#vote").addEventListener("click", (e)=>{
-    //ボタンが本来持っている処理をなかったことにする
-    e.preventDefault();
-    var input_num=document.forms.nums.value;
 
-    //ここで桁数ごとにわける
-    var n1 = Math.floor(input_num / 1) % 10; //1桁目
-    var n2 = Math.floor(input_num / 10) % 10; //2桁目
-    var n3 = Math.floor(input_num / 100) % 10; //3桁目
-    var n4 = Math.floor(input_num / 1000) % 10; //4桁目
+document.addEventListener("DOMContentLoaded", (e)=>{
+    document.getElementById('vote').addEventListener("click", (e)=>{
+        //ボタンが本来持っている処理をなかったことにする
+        e.preventDefault();
+        var input_num=document.forms.inputs.nums.value;
 
-    //ユニークかどうか
-    if(n1==n2 || n1==n3 || n1==n4 || n2==n3 || n2==n4 || n3==n4){
-        return;
-    }
+        //数字かどうか
+        if(!isNaN(input_num)){
+            alert("文字が入力されています");
+            return;
+        }
 
-    if(num==input_num){
-        //正解だったら
-    }else{
-        //間違いだったら
-    }
+        //桁数が４か
+        if(String(input_num).length!=4){
+            alert("４桁の数字を入力してください");
+            return;
+        }
     
-})
+        //ここで桁数ごとにわける
+        var n1 = Math.floor(input_num / 1000) % 10; //1桁目
+        var n2 = Math.floor(input_num / 100) % 10; //2桁目
+        var n3 = Math.floor(input_num / 10) % 10; //3桁目
+        var n4 = Math.floor(input_num / 1) % 10; //4桁目
+    
+        //ユニークかどうか
+        if(n1==n2 || n1==n3 || n1==n4 || n2==n3 || n2==n4 || n3==n4){
+            alert("ユニークな数字を入力してください");
+            return;
+        }
+        
+        console.log(n1,n2,n3,n4);
+
+        if(num==input_num){
+            //正解だったら
+        }else{
+            //間違いだったら
+        }
+        
+    })
+}, false);
