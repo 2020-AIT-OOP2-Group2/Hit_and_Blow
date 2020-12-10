cf5f4a2819d0ced5c861c1b03c9b9a582dfead5e
k19064 <ttt562777@icloud.com>
2020-12-10 15:29:03 +0900

    htmlとjsの変更
    基本的なhtmlの内容とjsの形だけ作成

--- static/playRequest.js
+++ static/playRequest.js
@@ -1,27 +1,34 @@
 var num;
+var play_count;
+var hit,blow;
 
-fetch("/address").then(response => {
+fetch("/start").then(response => {
     console.log(response);
     response.json().then((data) => {
         console.log(data);  // 取得されたレスポンスデータをデバッグ表示
-        data.forEach(elm => {
-            // 1行づつ処理を行う
-            let tr = document.createElement('tr');
-            // first name
-            let td = document.createElement('td');
-            td.innerText = elm.first_name;
-            tr.appendChild(td);
-            // last name
-            td = document.createElement('td');
-            td.innerText = elm.last_name;
-            tr.appendChild(td);
-            // email
-            td = document.createElement('td');
-            td.innerText = elm.email;
-            tr.appendChild(td);
-
-            // 1行分をtableタグ内のtbodyへ追加する
-            tableBody.appendChild(tr);
-        });
+        num=data.number;
     });
 });
+
+document.querySelector("#vote").addEventListener("click", (e)=>{
+    //ボタンが本来持っている処理をなかったことにする
+    e.preventDefault();
+    var input_num=document.forms.nums.value;
+    if(num==input_num){
+
+    }else{
+
+    }
+    
+})
+
+document.querySelector("#get_log").addEventListener("click", (e)=>{
+    //ボタンが本来持っている処理をなかったことにする
+    e.preventDefault();
+    fetch("/ranking").then(response => {
+        console.log(response);
+        response.json().then((data) => {
+            console.log(data);  // 取得されたレスポンスデータをデバッグ表示
+        });
+    });
+})
