af90fd45f3e72ef37850d4226a342c3bb28d3d2c
k19064 <ttt562777@icloud.com>
2020-12-10 15:31:51 +0900

    Update playRequest.js
    jsの内容修正

--- static/playRequest.js
+++ static/playRequest.js
@@ -6,7 +6,7 @@ fetch("/start").then(response => {
     console.log(response);
     response.json().then((data) => {
         console.log(data);  // 取得されたレスポンスデータをデバッグ表示
-        num=data.number;
+        num=data.number;　//数字を取得
     });
 });
 
@@ -15,20 +15,9 @@ document.querySelector("#vote").addEventListener("click", (e)=>{
     e.preventDefault();
     var input_num=document.forms.nums.value;
     if(num==input_num){
-
+        //正解だったら
     }else{
-
+        //間違いだったら
     }
     
-})
-
-document.querySelector("#get_log").addEventListener("click", (e)=>{
-    //ボタンが本来持っている処理をなかったことにする
-    e.preventDefault();
-    fetch("/ranking").then(response => {
-        console.log(response);
-        response.json().then((data) => {
-            console.log(data);  // 取得されたレスポンスデータをデバッグ表示
-        });
-    });
 })
