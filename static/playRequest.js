5263bebc60807451eba55a030c36f05d95c4bae1
k19064 <ttt562777@icloud.com>
2020-12-17 15:03:18 +0900

    エラーメッセージの追加

--- static/playRequest.js
+++ static/playRequest.js
@@ -4,6 +4,7 @@ var hit=0,blow=0;
 var n=new Array(4);
 var n_s=new Array(4);
 
+/**
 fetch("/start").then(response => {
     console.log(response);
     response.json().then((data) => {
@@ -11,6 +12,7 @@ fetch("/start").then(response => {
         num=data.number;　//数字を取得
     });
 });
+**/
 
 
 document.addEventListener("DOMContentLoaded", (e)=>{
@@ -19,15 +21,22 @@ document.addEventListener("DOMContentLoaded", (e)=>{
         e.preventDefault();
         var input_num=document.forms.inputs.nums.value;
 
+        if(input_num.length==0){
+            //alert("数字が入力されていません");
+            document.getElementById("error_message").innerHTML="数字が入力されていません";
+        }
+
         //数字かどうか
         if(!Number(input_num)){
-            alert("文字が入力されています");
+            //alert("文字が入力されています");
+            document.getElementById("error_message").innerHTML="文字が入力されています";
             return;
         }
 
         //桁数が４か
         if(String(input_num).length!=4){
-            alert("４桁の数字を入力してください");
+            //alert("４桁の数字を入力してください");
+            document.getElementById("error_message").innerHTML="４桁の数字を入力してください";
             return;
         }
 
@@ -41,7 +50,8 @@ document.addEventListener("DOMContentLoaded", (e)=>{
     
         //ユニークかどうか
         if(n[0]==n[1] || n[0]==n[2] || n[0]==n[3] || n[1]==n[2] || n[1]==n[3] || n[2]==n[3]){
-            alert("ユニークな数字を入力してください");
+            //alert("ユニークな数字を入力してください");
+            document.getElementById("error_message").innerHTML="ユニークな数字を入力してください";
             return;
         }
         
@@ -51,7 +61,9 @@ document.addEventListener("DOMContentLoaded", (e)=>{
         if(num==input_num){
             //正解だったら
             document.getElementById("judge_result").innerHTML="正解です";
+            document.getElementById("error_message").innerHTML="";
         }else{
+            document.getElementById("error_message").innerHTML="";
             //間違いだったら
             for(let j=0;j<4;j++){
                 for(let i=0;i<4;i++){
@@ -70,7 +82,8 @@ document.addEventListener("DOMContentLoaded", (e)=>{
 
             play_count++;
 
-            document.getElementById("judge_result").innerHTML=String(hit)+"Hit "+String(blow)+"blow "+"試行回数:"+String(play_count);
+            document.getElementById("judge_result").innerHTML=String(hit)+"Hit "+String(blow)+"blow";
+            document.getElementById("judge_log").innerHTML+="\n"+"回数: "+String(play_count)+" "+String(input_num)+" -> "+String(hit)+" Hit "+String(blow)+"blow";
 
             hit=0;
             blow=0;
