faedbe5096ef060e64ed4aec355a28fb2255870a
tikuwa <tikuwamk2@gmail.com>
2020-12-22 21:54:54 +0900

    moduleのインポートに不具合があったので修正しました

--- modules/ranking.py
+++ modules/ranking.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request, json, Flask
-from sql import dbctl
+from .sql import dbctl
+
 
 ranking = Blueprint('ranking', __name__)
 db = dbctl()
@@ -7,17 +8,18 @@ db = dbctl()
 
 @ranking.route('/ranking', methods=['GET', 'POST'])
 def ranking_response():
-
     if request.method == 'GET':
+
         json_data = db.getRanking()
         return jsonify(json_data)
+
     elif request.method == 'POST':
+
         if request.json == '':
             return jsonify({
                 'message': 'JSON not found.'
             })
 
-        print(request.json)
         db.postRanking(request.json['name'], request.json['moves'])
         return jsonify({})  # 表示できるページが存在しないというエラーが出るため，空のJSONを返す
 
